98c5ff1b88c35f7270e84a22b99f98a8a39b5377
Anurag33369 <74292266+Anurag33369@users.noreply.github.com>
2020-12-03 13:54:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var fixedRect, movingRect;
+var gameobj1, gameobj2;
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+  gameobj1 = createSprite(100,100,50,80);
+  gameobj2 = createSprite(800,100,50,80);
+  gameobj1.velocityX = 5;
+  gameobj2.velocityX = -5;
+  gameobj1.shapeColor = "yellow";
+  gameobj2.shapeColor = "red";
+}
+
+function draw() {
+  background(0,0,0);  
+
+  bounceOff(movingRect, fixedRect);
+  bounceOff(gameobj1, gameobj2)
+  drawSprites();
+}
+
